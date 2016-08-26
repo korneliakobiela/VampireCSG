0d8524946130b1b9f5bdb175e9797278af8ee27c
korneliakobiela <korneliak.95@gmail.com>
2016-08-27 01:35:00 +0200

    Init Commit

---
+++ js/userdata.js
@@ -0,0 +1,18 @@
+var userdata = (function () {
+    'use strict';
+    
+    function PlayerInput(cname, player, chronicle, nature, demeanor, clan, generation, haven, concept) {
+        this.cname = cname;
+        this.player = player;
+        this.chronicle = chronicle;
+        this.nature = nature;
+        this.demeanor = demeanor;
+        this.clan = clan;
+        this.generation = generation;
+        this.haven = haven;
+        this.concept = concept;
+        
+    }
+    //TODO: tests and methods to set and get
+
+}());
